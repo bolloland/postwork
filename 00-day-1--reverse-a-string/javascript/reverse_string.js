01fde8d2e695fb2b8cadd6ada38a7dc76524c664
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-02-28 20:09:24 -0600

    week 1

---
+++ 00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -0,0 +1,22 @@
+function reverseString(str) {
+  let rever = str.split("")
+  bob = rever.reverse().join("")
+  console.log(rever)
+  return bob
+}
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 'ih'");
+  console.log("=>", reverseString("hi"));
+
+  console.log("");
+
+  console.log("Expecting: 'ybabtac'");
+  console.log("=>", reverseString("catbaby"));
+}
+
+module.exports = reverseString;
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
